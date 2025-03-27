e73e6c115f112f00ba5b13efb5c80f744c6275ba
matheusfna <matheus_guga@hotmail.com>
2025-03-27 11:42:51 -0300

    Utilizando as técnicas de desenvolvimento: - Tecnica Placeholder - Tecnica Interpolação -Tecnica de Concatenação de Strings (antiga)

--- saidaDeDados/Program.cs
+++ saidaDeDados/Program.cs
@@ -13,7 +13,7 @@ namespace MyApp {
             Console.WriteLine("{0} tem {1} anos e possui um saldo bancário de R${2:F3} reais", nome, idade, saldo);
 
             // Utilizada a tecnica de interpolação
-            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a R${saldo:F3} reais");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a R${saldo:F3} reais"):
 
             //Utilizada a tecnica de concatenação de strings
             Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a R$" + saldo.ToString("F3", CultureInfo.InvariantCulture) + " reais");
