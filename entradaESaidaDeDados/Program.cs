8a1b6d33f27a617ab5d43a5ac036fc1f8231af8e
matheusfna <matheus_guga@hotmail.com>
2025-03-27 17:32:39 -0300

    Entrada e saida de dados utilizando vetor para amarzenar as variaveis de entrada de uma string, utilizando também a funcao split()

---
+++ entradaESaidaDeDados/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+
+namespace entradaESaidadeDeDados 
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+        {
+
+            //Leitura dos dados de entrada
+            string palavra1 = Console.ReadLine();
+            string palavra2 = Console.ReadLine(); 
+            string palavra3 = Console.ReadLine(); 
+            string palavra4 = Console.ReadLine();
+
+            //Vetor de string para pegar cada palavra dentro do vetor depois do espaço 
+            string[] s = Console.ReadLine().Split(' ');
+
+            string palavra5 = s[0]; 
+            string palavra6 = s[1]; 
+            string palavra7 = s[2];
+
+            //Saida dos dados de entrada mostrando na tela 
+            Console.WriteLine(palavra1);
+            Console.WriteLine(palavra2); 
+            Console.WriteLine(palavra3); 
+            Console.WriteLine(palavra4); 
+            Console.WriteLine(palavra5); 
+            Console.WriteLine(palavra6); 
+            Console.WriteLine(palavra7); 
+
+
+
+        }
+    }
+}
