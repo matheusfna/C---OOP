119164a27b23d47e78d66d8390bc12ceef2296cf
matheusfna <matheus_guga@hotmail.com>
2025-03-27 11:20:43 -0300

    Utilizando as técnicas de desenvolvimento: - Tecnica Placeholder - Tecnica Interpolação -Tecnica de Concatenação de Strings (antiga)

--- Curse/obj/Release/net9.0/Curse.AssemblyInfo.cs
+++ Curse/obj/Release/net9.0/Curse.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curse")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83abda793bc118bbdb1e07193aac00418082cfb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curse")]
---
+++ saidaDeDados/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double saldo = 10.35784;
+            int idade = 23;
+            string nome = "Maria Silva";
+
+            //Utilizada a tecnica de placeholder
+            Console.WriteLine("{0} tem {1} anos e possui um saldo bancário de R${2:F3} reais", nome, idade, saldo);
+
+            // Utilizada a tecnica de interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a R${saldo:F3} reais");
+
+            //Utilizada a tecnica de concatenação de strings
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a R$" + saldo.ToString("F3", CultureInfo.InvariantCulture) + " reais");
+
+        }
+    }
+}
