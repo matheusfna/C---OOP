13c1bd17c8f6434efe71848269ba75753d15228b
matheusfna <matheus_guga@hotmail.com>
2025-03-31 19:49:42 -0300

    Refatoração da classe Triangulo com a inclusão do metodo CalcularArea()

--- exemploSemPOO/Program.cs
+++ exemploSemPOO/Program.cs
@@ -7,7 +7,7 @@ namespace problemaExemploSemPOO {
         static void Main(string[] args) {
 
             Triangulo x, y; //Classe Triangulo
-            double areaX = 0, areaY = 0, p;
+            double areaX = 0, areaY = 0;
 
             x = new Triangulo(); //Instaciação da classe Triangulo
             y = new Triangulo(); //Instaciação da classe Triangulo
@@ -20,18 +20,12 @@ namespace problemaExemploSemPOO {
 
             Console.WriteLine("Entre com as medidas do triângulo Y: ");
             //Utilização do x. para atribuir a entrada de dados da classe Triangulo
-            y.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             y.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
              y.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
              y.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-
-            p = (x.A + x.B + x.C) / 2.0;
-
-            areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            p = (y.A + y.B + y.C) / 2.0;
-
-            areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+            areaX = x.CalcularArea();
+            areaY = y.CalcularArea();
 
             Console.WriteLine("Área de X: " + areaX.ToString("F4",CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y: " + areaY.ToString("F4",CultureInfo.InvariantCulture));
--- exemploSemPOO/Triangulo.cs
+++ exemploSemPOO/Triangulo.cs
@@ -1,11 +1,25 @@
-﻿
+﻿using System;
 
-namespace exemploSemPOO {
-    internal class Triangulo {
+namespace exemploSemPOO
+{
+    internal class Triangulo
+    {
 
         public double A;
         public double B;
         public double C;
+
+
+        public double CalcularArea(){
+
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+        }
+        
+
+
+        
 
     }
 }
