cf40b6c6f1a8ca3265b9cf69e1b886044a104a19
matheusfna <matheus_guga@hotmail.com>
2025-03-31 20:40:09 -0300

    Foi adicionado os metodos do tipo void para AdicionarProduto() e RemoverProduto() da classe Produto

--- problemaProduto/Produto.cs
+++ problemaProduto/Produto.cs
@@ -11,6 +11,14 @@ namespace problemaProduto {
         return Preco * Quantidade;
         }
 
+        public void AdicionarProduto(int quantidade) {
+        Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade) {
+        Quantidade -= quantidade;
+        }
+
         public override string ToString() {
         return Nome + ", $ " + Preco.ToString("F2",CultureInfo.InvariantCulture) + " , "
             + Quantidade + " unidades, Total: $ " 
--- problemaProduto/Program.cs
+++ problemaProduto/Program.cs
@@ -15,7 +15,24 @@ namespace problemaProduto {
         Console.Write("Quantidade no estoque: ");
         produto.Quantidade = int.Parse(Console.ReadLine());
 
+        Console.WriteLine();
         Console.WriteLine("Dados do produto: " + produto);
+
+        Console.WriteLine();
+        Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+        int qte = int.Parse(Console.ReadLine());
+        produto.AdicionarProduto(qte);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados Atualizados: " + produto);
+
+        Console.WriteLine();
+        Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+        qte = int.Parse(Console.ReadLine());
+        produto.RemoverProduto(qte);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados Atualizados: " + produto);
 
 
 
