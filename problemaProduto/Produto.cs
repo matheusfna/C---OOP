5954d4304c43000914bbfae846906440fab12814
matheusfna <matheus_guga@hotmail.com>
2025-03-31 20:27:03 -0300

    Solucionando um problema de Produtos, com a criação de uma classe, e os seus metodos

---
+++ problemaProduto/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace problemaProduto {
+    internal class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque() {
+        return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+        return Nome + ", $ " + Preco.ToString("F2",CultureInfo.InvariantCulture) + " , "
+            + Quantidade + " unidades, Total: $ " 
+            + ValorTotalEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+
+
+
+    }
+}
---
+++ problemaProduto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace problemaProduto {
+    internal class Program {
+        static void Main(string[] args) {
+
+        Produto produto = new Produto();
+
+        Console.WriteLine("Entre os dados do produto: ");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        produto.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Dados do produto: " + produto);
+
+
+
+
+
+
+        }
+    }
+}
+
+
