c62e20f588b84d205af27c1f91e45734c1466cac
matheusfna <matheus_guga@hotmail.com>
2025-03-31 18:47:02 -0300

    Exercicio resolvido com a criação de uma classe Pessoa, para verificar qual delas é a mais velha.

---
+++ exercicio01POO/Pessoas.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace exercicio01POO {
+    internal class Pessoa {
+
+        public string Nome;
+        public int Idade;
+
+    }
+}
---
+++ exercicio01POO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio01POO {
+    internal class Program {
+        static void Main(string[] args) {
+
+    //Classe Tipo -  nome  - instanciação da classe
+            Pessoa pessoa1 = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+           
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            pessoa1.Nome = Console.ReadLine();
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            pessoa2.Nome = Console.ReadLine();
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+
+            if(pessoa1.Idade > pessoa2.Idade) {
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.Nome);
+            } else {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.Nome);
+            }
+        }
+    }
+}
+
+
+
