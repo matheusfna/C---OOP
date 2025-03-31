a74a8151b349b5259adf82d783caf1da1c2347f3
matheusfna <matheus_guga@hotmail.com>
2025-03-31 14:50:33 -0300

    Pratica para criar funcoes e chamadas

---
+++ CriandoFuncoes/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace criandoFuncoes {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n1, n2, n3;
+
+
+            Console.WriteLine("Digite os três números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+
+
+            double resultado = Maior(n1,n2,n3);
+
+            Console.WriteLine("Maior = " + resultado);
+
+
+            //Funcao para verificar qual numero é maior dentre os digitados
+            static int Maior(int a,int b,int c) {
+                int m;
+                if(a > b && a > c) {
+                    m = a;
+                } else if(b > c) {
+                    m = b;
+                } else {
+                    m = c;
+                }
+                return m;
+
+
+
+            }
+        }
+    }
+}
+
