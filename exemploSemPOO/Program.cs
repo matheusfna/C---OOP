f9a08adc257be9f6e237567c8bb9aa7c9684cdff
matheusfna <matheus_guga@hotmail.com>
2025-03-31 18:00:22 -0300

    Refatoração de código para incluir a solução utilizando POO, com a criação da classe Triângulo.

--- exemploSemPOO/Program.cs
+++ exemploSemPOO/Program.cs
@@ -1,34 +1,37 @@
 ﻿using System;
 using System.Globalization;
+using exemploSemPOO;
 
 namespace problemaExemploSemPOO {
     internal class Program {
         static void Main(string[] args) {
 
-            double xA = 0, xB = 0, xC = 0;
-            double yA = 0, yB = 0, yC = 0;
+            Triangulo x, y; //Classe Triangulo
             double areaX = 0, areaY = 0, p;
 
-            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x = new Triangulo(); //Instaciação da classe Triangulo
+            y = new Triangulo(); //Instaciação da classe Triangulo
 
-            xA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            //Utilização do x. para atribuir a entrada de dados da classe Triangulo
+             x.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             x.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             x.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
             Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            //Utilização do x. para atribuir a entrada de dados da classe Triangulo
+            y.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             y.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             y.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-            yA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
+            p = (x.A + x.B + x.C) / 2.0;
 
-            p = (xA + xB + xC) / 2.0;
+            areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            p = (y.A + y.B + y.C) / 2.0;
 
-            p = (yA + yB + yC) / 2.0;
-
-            areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+            areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
             Console.WriteLine("Área de X: " + areaX.ToString("F4",CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y: " + areaY.ToString("F4",CultureInfo.InvariantCulture));
@@ -38,9 +41,6 @@ namespace problemaExemploSemPOO {
             } else {
                 Console.WriteLine("Maior área é: Y");
             }
-
-
-
         }
     }
 }
