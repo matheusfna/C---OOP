f85b6467743243e524e478c33955c4742eafd7cd
matheusfna <matheus_guga@hotmail.com>
2025-04-02 18:20:03 -0300

    Solucionar os calculos de entrada de dados de um funcionario com, nome, salarioBruto porcemtagem, realizando a criação da Classe Funcionario, e os metodos para realizar as operações e calculos.

---
+++ Exercicio2Funcionario/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio2Funcionario {
+    internal class Funcionario {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcemtagem) {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcemtagem / 100.0);
+        }
+
+        public override string ToString() {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio2Funcionario/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Exercicio2Funcionario {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + funcionario);
+
+            Console.Write("Digite a porcemtagem para aumentar o salário: ");
+            double porcemtagem = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcemtagem);
+
+            Console.Write("Dados atualizados: " + funcionario);
+
+        }
+    }
+}
+
+
