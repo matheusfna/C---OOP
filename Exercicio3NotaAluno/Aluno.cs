d98c4cf4f3aca2147f8c35743d5c10c06c78c870
matheusfna <matheus_guga@hotmail.com>
2025-04-02 20:31:51 -0300

    Solucão de programa para Notas de um aluno, para verificar se está apto a passar de ano, com a criação da classe aluno, e os metodos: NotaFinal() NotaQueFaltou()

---
+++ Exercicio3NotaAluno/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Exercicio3NotaAluno {
+    internal class Aluno {
+
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+
+        public double NotaFinal() {
+            return Nota1 + Nota2 + Nota3;        
+        }
+
+        public double NotaQueFaltou() {
+            return 60 - (Nota1 + Nota2 + Nota3);
+        }
+    }
+}
---
+++ Exercicio3NotaAluno/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3NotaAluno {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //INSTANCIA DA CLASSE ALUNO
+            Aluno aluno = new Aluno();
+
+            //ENTRADA DE DADOS DE NOME
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            //ENTRADA DE DADOS DAS NOTAS
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //NOTA FINAL COM CALCULO COM O METODO NotaFinal
+            Console.WriteLine("NOTA FINAL = " + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            //VERIFICACAO SE FOI APROVADO OU REPROVADO
+            if(aluno.NotaFinal() > 60.00) {
+                Console.WriteLine("APROVADO");
+            } else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + aluno.NotaQueFaltou().ToString("F2",CultureInfo.InvariantCulture) + " PONTOS ");
+            }
+        }
+    }
+}
+
+
+
