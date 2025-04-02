8a15d809acd53881b9c690ece6481586a128a83d
matheusfna <matheus_guga@hotmail.com>
2025-04-02 17:10:05 -0300

    Realizar a solução dos calculos de um retangulo, area, perimetro e diagonal utilizando uma classe Retangulo, e com a criação dos metodos da classe para realizar os calculos e as chamada dos metodos.

---
+++ Exercicio1Retangulo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using exercicio1Retangulo;
+
+namespace exercicio1Retangulo {
+    internal class Program {
+        static void Main(string[] args) {
+
+        //INSTANCIA DA CLASSE RETANGULO
+        Retangulo retangulo = new Retangulo();
+        
+        //ENTRADA E LEITURA DE DADOS
+        Console.WriteLine("Entre com a largura e altura do Retangulo: ");
+        //PARA FAZER A LEITURA DOS DADOS TEM QUE PUXAR A VARIAVEL DA CLASSE DEPOIS DE CRIAR A INSTANCIA DA CLASSE
+        retangulo.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        retangulo.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        Console.WriteLine();
+
+        //RESULTADO DO CALCULO DA AREA COM O METODO Area()
+        Console.WriteLine("AREA = " + retangulo.Area().ToString("F2",CultureInfo.InvariantCulture));
+
+        //RESULTADO DO CALCULO DO PERIMETRO COM O METODO Perimetro()
+        Console.WriteLine("PERÍMETRO = " + retangulo.Perimetro().ToString("F2",CultureInfo.InvariantCulture));
+
+        //RESULTADO DO CALCULO DA DIAGONAL COM O METODO Diagonal()
+        Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
+
+
---
+++ Exercicio1Retangulo/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio1Retangulo {
+    internal class Retangulo {
+
+        public double Altura;
+        public double Largura;
+
+        //Metodo para calcular a Area do Retangulo
+        public double Area() {
+        return Largura * Altura;
+        }
+
+        //Metodo para calcular a Perimetro do Retangulo
+        public double Perimetro() {
+        return 2.0 * (Largura + Altura);
+        }
+
+        //Metodo para calcular a Diagonal do Retangulo
+        public double Diagonal() {
+        return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+
+
+    }   
+}
