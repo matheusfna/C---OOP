8494933ac0e616deb1b1fcd2c9c7a81a9d7c9b5f
matheusfna <matheus_guga@hotmail.com>
2025-04-02 22:19:38 -0300

    Criado uma solução para conversor de Moedas, e utilizando membros estaticos da classe

---
+++ ConversorMoedasClassStatic/ConversorMoedas.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ConversorMoedasClassStatic {
+    internal class ConversorMoedas {
+
+        public static double IoF = 6.0;
+
+        public static double DolarParaReal(double quantidadeDolares,double cotacao) {
+            double total = quantidadeDolares * cotacao;
+            return total + total * IoF / 100.0;
+        }
+    }
+}
---
+++ ConversorMoedasClassStatic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoedasClassStatic {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //ENTRADA DE DADOS
+            Console.Write("Qual é a cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //ENTRADA DE DADOS
+            Console.Write("Quantos dólares você vai comprar: ");
+            double quantidadeDolares = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //REALIZANDO OS CALCULOS
+            double result = ConversorMoedas.DolarParaReal(quantidadeDolares,cotacao);
+
+            //SAIDA DE DADOS
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+
+
