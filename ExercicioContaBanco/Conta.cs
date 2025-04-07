30ffb3bd63c86ac2ff78a8be83be1174bd9a9bcc
matheusfna <matheus_guga@hotmail.com>
2025-04-07 14:57:20 -0300

    Resolução do problema realizado com sucesso, utilizando a classe Conta, para obter os atributos e metodos da classe, tais como:
    RealizarDeposito()
    RealizarSaque
    
    Utilizando também um override ToString para a atualização da conta após um novo deposito e saque.

---
+++ ExercicioContaBanco/Conta.cs
@@ -0,0 +1,58 @@
+﻿
+
+using System.Globalization;
+
+namespace ExercicioContaBanco {
+    internal class Conta {
+
+        private int _numeroConta;
+        private int _taxaBanco = 5;
+
+        public string NomeTitularConta {
+            get; private set;
+        }
+
+        public double Saldo {
+            get; private set;
+        }
+
+        //Construtores
+        public Conta() {
+        }
+
+        //Propriedades Customizadas
+
+        public Conta(int numeroConta,string nomeTitularConta,double saldo) {
+            _numeroConta = numeroConta;
+            NomeTitularConta = nomeTitularConta;
+            Saldo = saldo;
+            
+        }
+
+
+        //METODOS
+
+        public void RealizarDeposito(double depositoInicial) {
+            Saldo = Saldo + depositoInicial;
+        }
+
+        public void RealizarSaque(double saque) {
+            Saldo = Saldo - saque - _taxaBanco;
+        }
+
+        public override string ToString() {
+            return "Conta " + _numeroConta + ", Titular: " + NomeTitularConta + ", Saldo: $ "
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+
+
+        /*
+           - Atributos Privados
+           - Propriedades Autoimplementadas
+           - Construtores
+           - Propriedades customizadas
+           - Outros métodos da classe
+         */
+    }
+}
---
+++ ExercicioContaBanco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioContaBanco;
+internal class Program {
+    static void Main(string[] args) {
+
+
+
+        Console.Write("Entre com o número da conta: ");
+        int contaBanco = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre com o nome do titular da conta: ");
+        string nomeTitular = Console.ReadLine();
+
+        Console.Write("Havera deposito inicial (s/n)? ");
+        char resposta = char.Parse(Console.ReadLine());
+        if(resposta == 's') {
+            Console.Write("Entre com o valor do deposito inicial: ");
+            double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            resposta = (char) depositoInicial;
+        } else {
+            resposta = (char) 0.0;
+            
+        }
+        Conta conta = new Conta(contaBanco,nomeTitular,resposta);
+        Console.WriteLine();
+
+        Console.WriteLine("Dados da Conta: ");
+        Console.WriteLine(conta);
+
+        Console.WriteLine();
+
+        Console.Write("Entre com um valor para depósito: ");
+        double deposito = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+        conta.RealizarDeposito(deposito);
+        Console.WriteLine("Dados da Conta atualizados: ");
+        Console.WriteLine(conta);
+
+        Console.WriteLine();
+
+        Console.Write("Entre com um valor para saque: ");
+        double saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        conta.RealizarSaque(saque);
+        Console.WriteLine("Dados da Conta atualizados: ");
+        Console.WriteLine(conta);
+
+
+
+    }
+}
