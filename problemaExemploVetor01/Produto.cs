060f0f97a446c98953e36807cdee822ce3b50773
matheusfna <matheus_guga@hotmail.com>
2025-04-09 19:39:22 -0300

    Resolvendo um problema com vetores com objetos do tipo referencia, Parte 02 - VETORES

---
+++ problemaExemploVetor01/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace problemaExemploVetor01 {
+    class Produto {
+        
+        //ATRIBUTOS
+        private string Nome {
+            get; set;
+        }
+        private double Preco {
+            get; set;
+        }
+
+        //METODO GET
+        public string getNome() {
+            return Nome;
+        }
+
+        public double getPreco() {
+            return Preco;
+        }
+
+        //CONSTRUCTOR
+        public Produto() {
+        }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ problemaExemploVetor01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace problemaExemploVetor01 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] prod = new Produto[n];
+
+            for(int i = 0; i < n; i++) {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                prod[i] = new Produto(nome,preco);
+            }
+
+            double soma = 0;
+
+            for(int i = 0; i < n; i++) {
+                soma += prod[i].getPreco();
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("PRECO MEDIA IGUAL = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
