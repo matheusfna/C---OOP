af8daf88c30794784c70a41d8ad15d52604a09fe
matheusfna <matheus_guga@hotmail.com>
2025-04-15 11:04:48 -0300

    Adiciona sistema de aluguel de quartos com uso de POO, vetor fixo, controle por índice e leitura estruturada.

---
+++ exercicioFixacaoVetor02/Locatario.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace problemaExemploVetor01 {
+    class Locatario {
+
+        //ATRIBUTOS
+        public string Nome {
+            get; set;
+        }
+        public string Email {
+            get; set;
+        }
+
+        //Constructor
+        public Locatario(string nome,string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+        //Override metodo ToString
+        public override string ToString() {
+            return Nome + ", " + Email;
+
+        }
+
+    }
+}
+
---
+++ exercicioFixacaoVetor02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace problemaExemploVetor01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Locatario[] locatario = new Locatario[10];
+
+            Console.Write("Quantos quartos você quer alugar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++) {
+
+                Console.WriteLine($"Rent: #{i}:");
+                Console.Write("Nome: ");
+                string nomeLocatario = Console.ReadLine();
+                Console.Write("Email: " );
+                string emailLocatario = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int numQuarto = int.Parse(Console.ReadLine());
+                //Alocar na memoria depois dos dados passados de cada locatario
+                locatario[numQuarto] = new Locatario(nomeLocatario,emailLocatario);
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for(int i = 0; i < 10; i++) {
+                if(locatario[i] != null)
+                    //Esta pegando o numero do quarto dentro do loop FOR e 
+                    Console.WriteLine(i + ": " + locatario[i]);
+            }
+            
+        }
+
+
+
+    }
+}
+
