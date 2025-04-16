2d76e5de8347ef9ca8cfd731b910ea6bb4da4d3b
matheusfna <matheus_guga@hotmail.com>
2025-04-16 15:03:59 -0300

    Praticando e testando os conceitos de matriz no C#

---
+++ matrizC#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace matrizCSharp {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Declaração de matriz e sua instancia
+            double[,] matriz = new double[2,3];
+
+            // Pega o total de elementos dentro da matriz em todas as dimensoes
+            Console.WriteLine(matriz.Length);
+
+            // Para retornar a dimensao da matriz 
+            Console.WriteLine(matriz.Rank);
+
+            // Quantidade de linhas da matriz
+            Console.WriteLine(matriz.GetLength(0));
+
+            // Quantidade de colunas da matriz
+            Console.WriteLine(matriz.GetLength(1));
+
+        }
+    }
+}
