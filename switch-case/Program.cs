6965dfd62510c36e3990288e03a2711448f8ceb0
matheusfna <matheus_guga@hotmail.com>
2025-04-17 17:05:23 -0300

    sintaxe switch-case

---
+++ switch-case/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exercicioSwitchCase {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+
+            string day;
+
+            switch(x) {
+                case 1:
+                    day = "Segunda";
+                    break;
+                case 2:
+                    day = "Terça";
+                    break;
+                case 3:
+                    day = "Quarta";
+                    break;
+                case 4:
+                    day = "Quinta";
+                    break;
+                case 5:
+                    day = "Sexta";
+                    break;
+                case 6:
+                    day = "Sabado";
+                    break;
+                case 7:
+                    day = "Domingo";
+                    break;
+                default:
+                    day = "Invalid";
+                    break;
+            }
+
+            Console.WriteLine("Dia: " + day);
+        }
+    }
+}
